4b607e86b4d3ed0a3561b0e0a4de2a5edae3718a
Emanuelbahia <emanuelazcona@hotmail.com>
2023-06-06 14:19:37 -0300

    readme

--- readme.md
+++ readme.md
@@ -1,19 +1,4 @@
-<!-- react icons -->
+Portfolio
 
-npm install react-icons --save
-
-<!-- bootstrap -->
-
-npm install react-bootstrap bootstrap
-
-<!-- radix para hacer el menu-->
-
-npm install @radix-ui/react-menubar
-
-npm i react-router-dom
-
-npm install react-scroll
-
-<!-- email -->
-
-npm i @emailjs/browser
+Hi, my name is Emanuel Azcona and here I show you what my skills are,
+some projects I did recently and how you can communicate with me.
--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -13,12 +13,12 @@ export default function Body() {
         <h3 className="h3-name">Hello, my name is </h3>
         <h1 className="h1-name">Emanuel Azcona</h1>
         <h2 className="h2-name">I&apos;m Frontend Developer.</h2>
-        <p className="p-name">I&apos;m a person who likes to work in a team and <br/> i always open to new ideas and opinions. <br/> I like to create things with code, from small websites <br/> to complex web applications.</p>
+        <p className="p-name">I&apos;m a person who likes to work in a team and <br/> I always open to new ideas and opinions. <br/> I like to create things with code, from small websites <br/> to complex web applications.</p>
       </div>
       <section className="main-section about-section" id="about">
         <h2 className="main-h2"><span className="main-span">01.</span>About</h2>
         <div className="about-p-img">
-          <p className="p-about">Hi, my name is Emanuel and i love traveling, trekking and sports. <br/> My interest in web development started a few years ago.
+          <p className="p-about">Hi, my name is Emanuel and I love traveling, trekking and sports. <br/> My interest in web development started a few years ago.
             Now I enjoy creating attractive and innovative designs to make user interaction easier and the experience pleasant. <br/>
             I like to continue learning new technologies and tools to be able to solve problems and exchange ideas with the team because it&apos;s essential in this changing and evolitionary world.
           </p>
